cb02eaad114717347f394cc2b1d028cb41ed433a
Lio3307 <aureliofs01@gmail.com>
2026-07-26 23:11:55 +0700

    delete un used syntax and variable

--- components/dashboard/project-list.tsx
+++ components/dashboard/project-list.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
-import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
-import { EllipsisVertical, Pencil, Trash2 } from "lucide-react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
 export default function ProjectList() {
   return (
@@ -11,21 +9,6 @@ export default function ProjectList() {
         <CardHeader>
           <CardTitle>title</CardTitle>
           <CardDescription>Type: Docs</CardDescription>
-          {/*<CardAction>
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <EllipsisVertical size="18" className="hover:cursor-pointer" />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem>
-                  <Pencil /> Edit
-                </DropdownMenuItem>
-                <DropdownMenuItem variant="destructive">
-                  <Trash2 /> Delete
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </CardAction>*/}
         </CardHeader>
         <CardContent>
           <p className="text-muted-foreground text-sm">By: Name</p>
